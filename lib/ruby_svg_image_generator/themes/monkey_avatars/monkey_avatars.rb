f135ac036eb90f5274585375da8a7102b67e1f72
Franx0 <francisco.moya.martinez@gmail.com>
2016-04-20 10:26:18 +0200

    updated last changes from failed merge

--- lib/ruby_svg_image_generator/themes/monkey_avatars/monkey_avatars.rb
+++ lib/ruby_svg_image_generator/themes/monkey_avatars/monkey_avatars.rb
@@ -3,8 +3,8 @@ Dir[File.dirname(__FILE__)+'/*.rb'].each{ |f| require f }
 module RubySvgImageGenerator
   class MonkeyAvatars < RubySvgImageGenerator::Theme
 
-    def initialize options={}
-      super "monkey avatars", 13, 15, options
+    def initialize
+      super "monkey avatars", 13, 15
       @parts = [] << Background.new << Face.new << Skin.new << Makeup.new << Clothes.new << Eyes.new << Wearable.new << Mouth.new << Nose.new
     end
 
--- spec/ruby_svg_image_generator_spec.rb
+++ spec/ruby_svg_image_generator_spec.rb
@@ -1,24 +1,52 @@
 require 'spec_helper'
-
 describe RubySvgImageGenerator do
 
   it "dummy" do
     40.times do |i|
-      RubySvgImageGenerator.create_and_save "test_#{i}.svg" , "test_#{i}", :theme => RubySvgImageGenerator::T_HUMAN_AVATARS
+      RubySvgImageGenerator.create_random_and_save "test_#{i}.svg" , "test_#{i}", :theme => RubySvgImageGenerator::T_HUMAN_AVATARS
     end
+  end
 
-    RubySvgImageGenerator.create "test", :theme => RubySvgImageGenerator::T_HUMAN_AVATARS
-    RubySvgImageGenerator.create "test", :theme => RubySvgImageGenerator::T_TEST_THEME
+  describe "create_random" do
+    it "creates a svg string with a random picture of a theme" do
+      RubySvgImageGenerator::THEMES.each do |theme|
+        svg = RubySvgImageGenerator.create_random "test_image", :theme => theme
+        expect(svg).to be_a(String)
+        expect(svg).to include('<svg')
+      end
+    end
+  end
 
+  describe "create_random_and_save" do
+    it "creates a svg string with a selected picture of a theme" do
+      RubySvgImageGenerator::THEMES.each do |theme|
+        result = RubySvgImageGenerator.create_random_and_save "test_file.svg", "test_image", :theme => theme
+        expect(result).to be_truthy
+      end
+    end
+  end
 
+  describe "create" do
+    it "creates a svg string with a selected picture of a theme" do
+      RubySvgImageGenerator::THEMES.each do |theme|
+        theme_inst = RubySvgImageGenerator.get_theme_instance theme
+        parts = theme_inst.collect{|part| 0 }
+        svg = RubySvgImageGenerator.create "test_image", :theme => theme, :parts=>parts
+        expect(svg).to be_a(String)
+        expect(svg).to include('<svg')
+      end
+    end
+  end
 
-    ha = RubySvgImageGenerator::HumanAvatars.new
-
-    ha.each{|part| puts "=======>>" + part.name }
-
-    p ha.get_random_matrix
-
-    expect(1).to eq(1)
+  describe "create_and_save" do
+    it "creates a svg string with a selected picture of a theme" do
+      RubySvgImageGenerator::THEMES.each do |theme|
+        theme_inst = RubySvgImageGenerator.get_theme_instance theme
+        parts = theme_inst.collect{|part| 0 }
+        result = RubySvgImageGenerator.create_random_and_save "test_file.svg", "test_image", :theme => theme, :parts=>parts
+        expect(result).to be_truthy
+      end
+    end
   end
 
 end
